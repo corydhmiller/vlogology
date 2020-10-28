939eb60d51bff2343af885dc17d92bb2495d70a5
Cory Miller <corydhmiller@gmail.com>
2020-10-28 11:05:04 -0500

    Update settings

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2020: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  plugins: ['@typescript-eslint', 'react', 'prettier', 'jam3'],
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'import/extensions': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': ['error', { custom: 'ignore' }],
+    'prettier/prettier': 'error',
+    'jam3/no-sanitizer-with-danger': [
+      2,
+      {
+        wrapperName: ['your-own-sanitizer-function-name']
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['~']
+      }
+    }
+  }
+};
