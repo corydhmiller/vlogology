dbe65d22f0cc97bee9a5bdde616bbad2981d8f99
Cory Miller <corydhmiller@gmail.com>
2021-01-03 10:38:07 -0600

    Rename blockquote lmao

--- components/ui/Blockquote.tsx
+++ components/ui/Blockquote.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-const Button = ({ children }: { children: React.ReactNode }) => {
+const Blockquote = ({ children }: { children: React.ReactNode }) => {
   return (
     <blockquote className="px-4 mx-4 my-8 text-lg italic font-bold text-gray-600 sm:text-2xl lg:my-16">
       {children}
@@ -8,4 +8,4 @@ const Button = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default Button;
+export default Blockquote;
