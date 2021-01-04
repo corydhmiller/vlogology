93a347b68e0d0f8e602176aa7189996bd8b4b08b
Cory Miller <corydhmiller@gmail.com>
2021-01-03 20:00:06 -0600

    Add telling your story

---
+++ pages/story/telling-your-story.tsx
@@ -0,0 +1,303 @@
+import * as React from 'react';
+import Link from 'next/link';
+import Page from '../../components/Page';
+// UI
+import CopyBlock from '../../components/ui/CopyBlock';
+import Button from '../../components/ui/Button';
+import Heading from '../../components/ui/Heading';
+import Blockquote from '../../components/ui/Blockquote';
+
+const YourGreatestAdvantage = () => {
+  const nextPage = '/story/getting-in-front-of-the-camera/';
+
+  return (
+    <Page
+      title="Telling your story"
+      image="/images/pages/headers/telling-your-story.jpg"
+    >
+      <CopyBlock>
+        <p>
+          When you make a vlog and people start watching it, an amazing thing
+          happens.
+        </p>
+
+        <p>
+          It’s the same thing that happens when you have a conversation, when
+          you sit and watch a movie, or when you read a phenomenal book you
+          simply can’t put down: <strong>a story is being told.</strong>
+        </p>
+
+        <p>
+          Regardless of the <em>quality</em> of the story, it is being told
+          nonetheless. Information is being transferred, worldview is being
+          shared, and an idea is spread beyond just one person.
+        </p>
+
+        <p>
+          Before modern technology, stories were passed down from generation to
+          generation through the spoken word in an attempt to preserve culture
+          and way of life. Spoken stories were joined with written stories,
+          whether through cave paintings or runes scratched into parchment.
+        </p>
+
+        <Blockquote>
+          <strong>Telling a story is nothing new,</strong> there are just more
+          ways to do it now.
+        </Blockquote>
+
+        <p>
+          Making a vlog is telling a story. It might not be a <em>literal</em>{' '}
+          “story” with chapters and all that, but it is the spread of an idea,
+          the sharing of perspective, and the invitation to something more.
+        </p>
+
+        <p>
+          Many people are uncomfortable to get in front of a camera (we’ll talk
+          about that next) and one of the primary reasons is because they’re not
+          sure what to do or say. We get weird in front of a camera! So what do
+          we do about that?
+        </p>
+
+        <p>
+          Telling your story (or <em>a</em> story) can come in a multitude of
+          different ways, but I’ll give you two simple examples today on how you
+          can craft a story through your vlog.
+        </p>
+
+        <Heading level="h2">Tell a story…literally</Heading>
+
+        <p>
+          I wrote on a previous page about how your greatest advantage in the
+          world is that no one else has lived your life.
+        </p>
+
+        <p>
+          This means{' '}
+          <strong>
+            any experience you’ve ever had is a story that can be shared.
+          </strong>
+        </p>
+
+        <p>
+          To be clear, you <em>shouldn’t</em> share everything in your life.
+          There are memories and moments that are special and private, and you
+          need to keep those for yourself. But if you’re not sure what to share
+          about, you can literally tell a story from your life!
+        </p>
+
+        <p>
+          There are days and weeks where I don’t feel like vlogging. Either the
+          situation in my life seems a bit bland or maybe I just simply don’t
+          feel like vlogging.
+        </p>
+
+        <p>
+          These are the days where{' '}
+          <strong>I pick up my camera and tell a story.</strong>
+        </p>
+
+        <p>Here are a few tips for simple storytelling from memory:</p>
+        <ul className="mb-4">
+          <li>
+            Share an experience or moment you had in the past as you remember
+            it. The more descriptive you can be, the better.
+          </li>
+          <li>
+            Be sure to <strong>give context to the story</strong> and not simply
+            give the punchline right away.
+          </li>
+          <li>
+            Visual aids are a <em>great</em> asset if you have them. Photos,
+            items, anything that may have been connected.
+          </li>
+        </ul>
+
+        <p>
+          It’s true that{' '}
+          <strong>storytelling is an art that requires practice,</strong> but if
+          you’ve ever shared anything about your life with anyone else, you’ve
+          already done it.
+        </p>
+
+        <p>
+          Note that this can be from any point in time, even five minutes ago!
+          It doesn’t have to be something from way back in your past, it could
+          have been from yesterday.
+        </p>
+
+        <p>
+          This is a great way to engage with your audience and is simply the
+          easiest way to start getting vlogs made.
+        </p>
+
+        <Heading level="h2">Show, don’t just tell</Heading>
+
+        <p>
+          The other great way to tell a story is through the situations and
+          environments you will actually record.
+        </p>
+
+        <p>
+          Most vloggers grab the camera and point it at themselves and talk, and
+          while that’s fine, you can{' '}
+          <strong>
+            give greater context and add significant interest to your vlogs
+          </strong>{' '}
+          by showing it.
+        </p>
+
+        <p>
+          One of the easiest examples of this is done through the much overused
+          (yes, I do it too) downward shot of feet walking. You point your
+          camera down while you’re going somewhere and record your feet.
+        </p>
+
+        <p>
+          This may seem kind of silly at first, but what is this shot saying?{' '}
+          <strong>It says they’re going somewhere.</strong> The viewer{' '}
+          <em>immediately</em> knows that a transition in location is happening.
+        </p>
+
+        <p>
+          Transitions like this give greater context to what is happening and
+          allows the viewer to move along with you.
+        </p>
+
+        <p>
+          Film directors use these kinds of transitions all the time. There’s a
+          scene happening in a city where the characters are talking about going
+          on a hike in the mountains, and in the last shot of the scene the
+          camera pans up toward the distant mountains. The next shot following
+          is maybe an aerial shot of the mountain forests, or of the car
+          barreling down the highway through the mountains.
+        </p>
+
+        <p>Another example is one I’ve used:</p>
+
+        <ul className="mb-4">
+          <li>
+            The camera tilts down at the floor and there’s Coco Pops everywhere.
+          </li>
+          <li>
+            The camera walks a bit further and there’s a spoon and a bowl spread
+            out away from the cereal.
+          </li>
+          <li>
+            After a pause, the camera tilts up to spot a little figure sitting
+            by the window.
+          </li>
+          <li>
+            The final move goes down toward the child, who turns and smiles with
+            food residue all over her face.
+          </li>
+        </ul>
+
+        <p>
+          Without words, I’ve communicated that there’s a giant mess and that{' '}
+          <em>someone</em> is responsible. With a bit of pacing, this can be a
+          very humorous moment for viewers of your vlog.
+        </p>
+
+        <p>
+          We’re still telling a story here, but in a way the viewer is in
+          control of how they are perceiving the story. They are coming up with
+          the words in their head as you guide them along.
+        </p>
+
+        <p>
+          Showing is definitely one of my favorite ways to tell story, and while
+          it takes a bit more practice than simply speaking, it can be a great
+          way to improve your vlogs and connect with your audience.
+        </p>
+
+        <Heading level="h2">The story can be about anything</Heading>
+
+        <p>
+          Maybe you don’t want to vlog about your life. Maybe you want to vlog
+          about your business or the organization you work for.
+        </p>
+
+        <p>
+          These principles still apply no matter the subject. Sharing stories
+          about work you’ve done for clients or bringing the camera along to
+          fundraisers and events are great ways to communicate to your viewers
+          what you’re about.
+        </p>
+
+        <p>
+          Making a vlog for your business or organization can be one of your
+          greatest marketing assets, as well. Many companies operate behind a
+          shroud of secrecy and privacy, resulting in a disconnect between them
+          and the customer.
+        </p>
+
+        <p>
+          By opening up the doors a little bit, you can demonstrate that the
+          people in the company are <em>real</em> people just like their
+          customers, and that can make all the difference.
+        </p>
+
+        <Heading level="h2">Quick thought on storytelling</Heading>
+
+        <p>
+          I knew that as I wrote this section there would be many things left
+          unsaid about storytelling. I admittedly left a lot of things out:
+        </p>
+
+        <ul className="mb-4">
+          <li>The hero’s journey</li>
+          <li>Conflict and resolution</li>
+          <li>Escalation and climax</li>
+          <li>Demonstrating change</li>
+          <li>And many other things…</li>
+        </ul>
+
+        <p>
+          “Where are the legendary X Rules of Storytelling? Where are the guides
+          on how to ramp up conflict and resolve it by the end of a seven-minute
+          video? Where are the…”
+        </p>
+
+        <p>
+          The reason I left out all of these things in this particular context
+          is for two reasons:
+        </p>
+
+        <ul className="mb-4">
+          <li>
+            Vlogology is primarily focused on helping you get started with
+            vlogging.
+          </li>
+          <li>
+            There are already <em>many</em> resources and videos out there on
+            how to tell better stories.
+          </li>
+        </ul>
+
+        <p>
+          Many people feel stuck when they think about vlogging. I’m not here to
+          showcase how to become the next storytelling master. I’m here to help
+          you get started.
+        </p>
+
+        <p>
+          Improvement will come and you’ll find your own story flow.{' '}
+          <strong>
+            Getting started is more important than getting it “perfect”
+          </strong>
+          .
+        </p>
+
+        <div className="mt-4 text-center">
+          <Link href={nextPage}>
+            <a href={nextPage}>
+              <Button color="yellow">Continue Reading</Button>
+            </a>
+          </Link>
+        </div>
+      </CopyBlock>
+    </Page>
+  );
+};
+
+export default YourGreatestAdvantage;
