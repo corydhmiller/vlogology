417fd0c1a17e347a7a6df49ede38b4ea13d3629b
Cory Miller <corydhmiller@gmail.com>
2021-01-05 09:44:28 -0600

    Add Fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+
+import * as Fathom from 'fathom-client';
 
 // Load in Tailwind first to provide base styles
 import '../styles/tailwind.css';
@@ -9,6 +12,26 @@ import '../styles/styles.scss';
 import '../public/font/fonts.css';
 
 function VlogologyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    Fathom.load('PBECBCLF', {
+      includedDomains: ['https://vlogology.com']
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
